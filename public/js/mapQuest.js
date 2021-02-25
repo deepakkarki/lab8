1323f3656b2ff0480b395357175c393294641060
deepakkarki <deepak6k@gmail.com>
2021-02-25 23:15:00 +0530

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'buYUhbpk4Um0GI2ClyoOncIdAxenan0C';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [37.7749, -122.4194],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([37.7749, -122.4194]).addTo(map);
 }
